c8404ff5adaa54658930630a6fbd9c61631ab613
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-02-23 23:44:39 +0530

    Update animator controller and player controller script

--- Assets/Scripts/PlayerControllerNetwork.cs
+++ Assets/Scripts/PlayerControllerNetwork.cs
@@ -10,18 +10,43 @@ public class PlayerControllerNetwork : NetworkBehaviour
     private PlayerInput playerInput;
     private CharacterController characterController;
     private Animator anim;
+    private float turnSmoothTime=0.1f;
+    private float turnSmoothVelocity;
+    private float playerSpeed = 10f;
+    private float dampTime=0.02f;
     
     // Start is called before the first frame update
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
-        characterController = GetComponentInChildren<CharacterController>();
+        characterController = GetComponent<CharacterController>();
         anim=GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        Vector2 inputVector = playerInput.actions["Move"].ReadValue<Vector2>();
+        float horizontal = inputVector.x;
+        float vertical = inputVector.y;
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDirection * playerSpeed * Time.deltaTime);
+            anim.SetFloat("moveX",moveDirection.x,dampTime,Time.deltaTime);
+            anim.SetFloat("moveZ",moveDirection.z,dampTime,Time.deltaTime);
+        }
     }
 }
