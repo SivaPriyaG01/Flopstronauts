78deea9ec90f36e7b844270cb618ed0f960983da
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-04-13 23:32:04 +0530

    Update UI Manager Main menu button script

--- Assets/Scripts/DelLaterMainMenuButtons.cs
+++ Assets/Scripts/DelLaterMainMenuButtons.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class DelLaterMainMenuButtons : MonoBehaviour
+public class DelLaterMainMenuButtons : NetworkBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        
+        await UnityServices.InitializeAsync();
+
+        if(!AuthenticationService.Instance.IsSignedIn)
+        {
+            try
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+            catch(AuthenticationException e)
+            {
+                Debug.Log(e);
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
+
+    public void OnExitButtonClick()
     {
-        
+        Application.Quit();
     }
 }
