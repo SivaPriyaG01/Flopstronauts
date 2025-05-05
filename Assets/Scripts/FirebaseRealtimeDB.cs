8089bfc3980b90804d1ece072130e2dfc1d2bcdb
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-05-05 15:42:39 +0530

    Update FirebaseRealtimeDB.cs

--- Assets/Scripts/FirebaseRealtimeDB.cs
+++ Assets/Scripts/FirebaseRealtimeDB.cs
@@ -7,33 +7,80 @@ using Firebase.Extensions;
 
 public class FirebaseRealtimeDB : MonoBehaviour
 {
+    DatabaseReference dbRef;
     string firebaseURL="https://flopstronauts-default-rtdb.asia-southeast1.firebasedatabase.app/";
 
 
+    [System.Serializable]
     public class User {
     public string username;
-    public string playerID;
+    //public string playerID;
 
     public User() {
     }
 
-    public User(string username, string playerID) {
-        this.username = username;
-        this.playerID = playerID;
-    }
+        public User(string username)
+        {
+            this.username = username;
+        }
+
+    //     public User(string username, string playerID) {
+    //     this.username = username;
+    //     // //this.playerID = playerID;
+    //     // public User(string username)
+    //     // {
+    //     //     this.username = username;
+    //     // }
+    // }
 }
     
     void Awake()
     {
-        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        //DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
-    public void WriteNewUser(string userId, string playerID) 
+    void Start()
     {
-    User user = new User(name, playerID);
+        
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            if (task.Result == DependencyStatus.Available)
+            {
+                // Initialize Firebase
+                FirebaseApp app = FirebaseApp.DefaultInstance;
+
+                // Database URL
+                app.Options.DatabaseUrl = new System.Uri(firebaseURL);
+
+                // Initialize FirebaseAuth and DatabaseReference
+                dbRef = FirebaseDatabase.DefaultInstance.RootReference;
+
+                Debug.Log("Firebase initialized successfully.");
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies: " + task.Result);
+            }
+        });
+    }
+
+    public void WriteNewUser(string username,string playerID) 
+    {
+    //User user = new User(username, playerID);
+    User user = new User(username);
     string json = JsonUtility.ToJson(user);
 
-    //mDatabaseRef.Child("users").Child(userId).SetRawJsonValueAsync(json);
+    dbRef.Child("users").Child(playerID).SetRawJsonValueAsync(json).ContinueWithOnMainThread(task=>
+    {
+        if(task.IsCompleted &&!task.IsFaulted)
+        {
+            Debug.Log("Player Username Saved");
+        }
+        else
+        {
+            Debug.Log("Save failed");
+        }
+    });
     }
 
 
