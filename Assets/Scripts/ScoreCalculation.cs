e2153e7ccae105fea75574f1921bcf2bd9e864e3
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-05-14 22:36:44 +0530

    Add ScoreCalculation.cs script
    Script to calculate players score after a session

---
+++ Assets/Scripts/ScoreCalculation.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCalculation : MonoBehaviour
+{
+    int playerScore=150;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Obstacle":
+            UpdatePlayerScore(-5);
+            break;
+
+            case "Ground":
+            UpdatePlayerScore(-10);
+            break;
+
+            case "FinishLine":
+            UpdatePlayerScore(20);
+            break;
+        }    
+    }
+
+    void UpdatePlayerScore(int score)
+    {
+        playerScore+=score;
+    }
+}
